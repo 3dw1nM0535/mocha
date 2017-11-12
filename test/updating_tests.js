df6f50a14d8c3789d0b9e88c4b914cf839813e85
3dw1nM0535 <workockmoses@gmail.com>
2017-11-13 00:41:37 +0300

    implemented delete tests run

---
+++ test/updating_tests.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+var MarioChar = require('../models/mariochar');
+
+//describe test run
+describe('Update Records in DB', function () {
+
+  //hook beforeEach() to save record to update
+  var char;
+  beforeEach(function (done) {
+
+    char = new MarioChar({
+      name: 'Brian Scotch',
+      weight: 38
+    });
+
+    char.save().then(function () {
+      done();
+    });
+
+  });
+
+  //create it block tests run
+  it ('Updating a record in the db', function (done) {
+    MarioChar.findOneAndUpdate({name: 'Brian Scotch'}, {name: 'Boss Budgy'}).then(function () {
+      MarioChar.findOne({_id: char._id}, function (record) {
+        assert(record.name === 'Boss Budgy');
+        doone();
+      })
+    })
+  });
+
+
+});
